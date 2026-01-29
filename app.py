bc24064d29965dddf3910d57afc2c61b102a47f1
MrRat0 <alons2080@gmail.com>
2026-01-29 17:11:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# RUTA PRINCIPAL (Inicio)
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# RUTA DE VIDEOS
 @app.route('/videos')
 def videos():
-    # Renderizamos la misma página pero el JS se encargará de bajar a la sección
-    return render_template('index.html')
+    # Aquí pasamos el ID del canal de Nemo para que el JS haga la magia
+    return render_template('videos.html', channel_id="UCpP_S_m7_N7O0yX_D9B4w9Q")
 
-# RUTA DE COMUNIDAD / SOCIALS
-@app.route('/comunidad')
-def comunidad():
-    return render_template('index.html')
+@app.route('/directos')
+def directos():
+    return render_template('directos.html')
 
-# CONFIGURACIÓN PARA DESPLIEGUE REAL (RENDER)
 if __name__ == '__main__':
-    # Render asigna dinámicamente un puerto a través de la variable de entorno PORT
-    # Si no existe (como en tu PC), usará el 704 por defecto
     port = int(os.environ.get("PORT", 704))
-    
-    # host='0.0.0.0' es obligatorio para que el servidor sea accesible desde afuera
     app.run(host='0.0.0.0', port=port)
