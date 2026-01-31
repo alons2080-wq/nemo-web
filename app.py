6deaa2ea4a9edf2d5520f30c213930f0c713c08d
MrRat0 <alons2080@gmail.com>
2026-01-31 12:29:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from supabase import create_client, Client
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'nemo_704_ultra_secret'
-
-SUPABASE_URL = "https://qsowpgaiqkduzgypwpww.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzb3dwZ2FpcWtkdXpneXB3cHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MTUzMjYsImV4cCI6MjA4NTM5MTMyNn0.o9hOYTdXw-yvai7XMf5-ZiIaGq5r9aogTKTgqzm8C0c" 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def index():
@@ -14,13 +8,7 @@ def index():
 
 @app.route('/videos')
 def videos():
-    try:
-        res = supabase.table('posts').select("*").not_.is_('video_url', 'null').execute()
-        posts = res.data
-    except:
-        posts = []
-    return render_template('videos.html', posts=posts)
+    return render_template('videos.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, port=704)
